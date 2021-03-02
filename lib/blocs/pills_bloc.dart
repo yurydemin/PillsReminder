fbf4973b5410554796301d8c61fd334ee9e6cf17
yuriy_demin <y.y.demin@gmail.com>
2021-03-02 09:10:27 +0500

    add event model and bloc

--- lib/blocs/pills_bloc.dart
+++ lib/blocs/pills_bloc.dart
@@ -1,1 +1,17 @@
-class PillsBloc {}
+import 'package:pills_reminder/models/pills_event.dart';
+import 'package:rxdart/subjects.dart';
+
+class PillsBloc {
+  final _events = BehaviorSubject<List<PillsEvent>>();
+
+  // Getters
+  Stream<List<PillsEvent>> get events => _events.stream;
+
+  // Setters
+  Function(List<PillsEvent>) get changeEvents => _events.sink.add;
+
+  // Dispose
+  dispose() {
+    _events.close();
+  }
+}
--- lib/models/pills_event.dart
+++ lib/models/pills_event.dart
@@ -1,1 +1,14 @@
-class PillsEvent {}
+import 'package:flutter/material.dart';
+
+class PillsEvent {
+  final UniqueKey id;
+  final TimeOfDay time;
+  final String description;
+  bool isActive;
+
+  PillsEvent({this.id, this.time, this.description, this.isActive});
+
+  activate() {
+    isActive = !isActive;
+  }
+}
